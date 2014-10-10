1d6c7e5fa00012c313cc37f23f97a4c4f834f420
Zack Grossbart <zack@grossbart.com>
2014-10-10 14:30:22 -0400

    We can now show the files in a simple graph.

--- busfactor.js
+++ busfactor.js
@@ -10,12 +10,18 @@ var busfactor = {
             for (var i = logs.length - 1; i >= 0; i--) {
                 for (var j = 0; j < logs[i].files.length; j++) {
                     var fileName = logs[i].files[j].name;
+                    if (fileName.indexOf('README.md') === 0) {
+                        // We don't care about the README.md file
+                        continue;
+                    }
+                    
                     if (!files[fileName]) {
                         var file = {};
                         file.committers = {};
                       
                         files[fileName] = file;
                         file.name = fileName;
+                        file.commitCount = 0;
                     }
                     
                     if (logs[i].files[j].action === bflog.DELETE) {
@@ -39,14 +45,14 @@ var busfactor = {
             
             busfactor.drawFiles(files, sortedFiles);
             
-/*            for (var i = 0; i < sortedFiles.length; i++) {
+            for (var i = 0; i < sortedFiles.length; i++) {
                 var file = files[sortedFiles[i]];
-                console.log('The file (' + file.name + ') was edited by');
+                console.log('The file (' + file.name + ') was edited ' + file.commitCount + ' time(s) by');
                 _.each(file.committers, function(committer) {
                     console.log('    ' + committer.author + ' ' + committer.count + ' time(s)');
                 });
             }
-*/
+
         });
     },
     
@@ -57,11 +63,27 @@ var busfactor = {
         var min = Object.keys(files[sortedFiles[sortedFiles.length - 1]].committers).length;
         var bump = Math.floor(height / ((max - min) + 1));
         
+        // We need to find the maximum and minumum number of commits for
+        // the files we're dealing with.  We do that by sorting the files
+        // by the number of committers.
+        
+        var filesByCommitNum = sortedFiles.slice();
+        filesByCommitNum = _.sortBy(filesByCommitNum, function(fileName) {
+            return files[fileName].commitCount;
+        });
+        var maxCommitters = files[filesByCommitNum[filesByCommitNum.length - 1]].commitCount;
+        var minCommitters = files[filesByCommitNum[0]].commitCount;
+        var commitBump = Math.floor(width / ((maxCommitters - minCommitters) + 1));
+        
         console.log('height: ' + height);
         console.log('width: ' + width);
         console.log('max: ' + max);
         console.log('min: ' + min);
         console.log('bump: ' + bump);
+        console.log('maxCommitters: ' + maxCommitters);
+        console.log('minCommitters: ' + minCommitters);
+        console.log('commitBump: ' + commitBump);
+        
         
         // We'll start by drawing the counts at the side of the graph
         for (var i = min; i <= max; i++) {
@@ -87,13 +109,13 @@ var busfactor = {
         
         // Now we'll add the files
         _.each(sortedFiles, function(fileName) {
-            console.log('fileName: ' + fileName);
             var file = files[fileName];
             
             var fileLabel = $('<div class="busGraphItem">' + fileName + '</div>');
             var r = busfactor.getRandomArbitrary(0, bump - (bump / 3));
             fileLabel.css('top', (((height - (((Object.keys(file.committers).length - 1) * bump))) - bump) + r) + 'px');
-            fileLabel.css('left', busfactor.getRandomArbitrary(0, width - (width / 3)) + 'px');
+            fileLabel.css('left', ((file.commitCount - 1) * commitBump) + 'px');
+            console.log(fileName + ' had a commit count of ' + file.commitCount);
             $('#busGraph').append(fileLabel);
         });
         
@@ -131,6 +153,7 @@ var busfactor = {
         }
         
         committer.count = committer.count + 1;
+        file.commitCount = file.commitCount + 1;
     }
     
 };
